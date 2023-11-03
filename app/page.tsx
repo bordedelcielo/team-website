cb50f45b1fc1d7d36d96f6164cf07a8ec59a9f5e
bordedelcielo <christopher.thrutchley@gmail.com>
2023-11-02 23:48:26 -0500

    Experiment with tailwind buttons

--- app/page.tsx
+++ app/page.tsx
@@ -110,6 +110,9 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+        <button class="bg-cyan-500 shadow-lg shadow-cyan-500/50 hover:shadow-indigo-500/50">Subscribe</button>
+        <button class="bg-blue-500 shadow-lg shadow-blue-500/50">Subscribe</button>
+        <button class="bg-indigo-500 shadow-lg shadow-indigo-500/50 hover:shadow-white">Subscribe</button>
       </div>
     </main>
   )
